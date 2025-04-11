0a6fc22b1f384a925a69ea4e5b12ccb4973179c3
kalthom <kalthum.mufath@gmail.com>
2025-04-11 11:23:44 +0300

    create anything

---
+++ kalthum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#define ll long long
+
+signed main()
+{
+    ios_base::sync_with_stdio(sync: 0), cin.tie(tiestr: 0 ), cout.tie(tiestr: 0);
+
+    return 0;
+}
